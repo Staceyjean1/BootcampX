a50dd0743883c409c4a505b67de2ee3c0f7874ae
Stacey Burke <stacey.burke@telus.com>
2023-01-01 23:16:25 -0800

    add paramaterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,22 +1,33 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
+  user: 'labber',
+  password: 'labber',
   host: 'localhost',
+  port: 5432,
   database: 'bootcampx'
 });
 
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-.then(res => {
-  console.log(res);
-})
-.catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values);
